d980e1727c988e52c5970f87134a2e275494c833
RubenGtz0 <gutierrezruben.000@gmail.com>
2023-05-09 14:09:30 -0600

    Pagina de Bienvenida

---
+++ Welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sport Shop Online</title>
+    <!-- Agrega aquí los estilos CSS o enlaces a archivos de estilo externos si es necesario -->
+</head>
+<body>
+    <div class="container">
+        <h1>Sport Shop Online</h1>
+        <a href="index.php"><button>Ir al Catálogo Principal</button></a>
+    </div>
+</body>
+</html>
