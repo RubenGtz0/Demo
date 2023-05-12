a47d68479943c8f95938c26623e361b63626e558
RubenGtz0 <gutierrezruben.000@gmail.com>
2023-05-12 01:03:02 -0600

    Act-1205

--- Welcome.php
+++ Welcome.php
@@ -1,13 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sport Shop Online</title>
-    <!-- Agrega aquí los estilos CSS o enlaces a archivos de estilo externos si es necesario -->
-</head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+    <style>
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 90vh;
+    }
+</style>
 <body>
-    <div class="container">
-        <h1>Sport Shop Online</h1>
-        <a href="index.php"><button>Ir al Catálogo Principal</button></a>
-    </div>
+        <!--aqui va el jumbo tron maybe-->
+        <!--dividir el sitio-->
+                    <div class="col-md-15 order-md-20">
+                    <h1>Sport Shop Online</h1>
+                    
+                    <p class="lead">Aprovecha las nuevas oferta por lanzamiento oficial de la pagina entra ahora!!</p>
+<br>
+<b>Características:</b><br>
+Marca: Cklass<br>
+Colección: Otoño-Invierno 2021<br>
+Modelo: 273-29<br>
+Color: Café<br>
+Material corte: Piel Natural Vacuno<br>
+Material Suela: Sintética<br></p>
+
+                    <div class="d-grid gap-5 col-3">
+                        <button class="btn btn-outline-primary" type="button" onclick="window.location.href=('index.php')">Ingresar</button>
+                    </div>
+                </div>
+           <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- webfonts/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer mt-auto py-2 bg-dark">
-    <div class="container">
-        <p class="text-center">
-            <span class="text-white">&copy; <?php echo date('Y'); ?> <a href="https://github.com/RubenGtz0/ProyectSist">Sports Shop Online</a></span>
-        </p>
-    </div>
-</footer>
