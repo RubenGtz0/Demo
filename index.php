bf19b482944eddf24f97d3ac6e813a6f6f929e92
RubenGtz0 <gutierrezruben.000@gmail.com>
2023-05-18 08:20:33 -0600

    indexConsolidado

--- index.php
+++ index.php
@@ -15,85 +15,39 @@ $comando->execute();
 $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
-<html lang="es" class="h-100">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda en linea</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="css/all.min.css" rel="stylesheet">
+    <title>Sport Shop Online</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/estilos.css" rel="stylesheet">
-</head>
-
-<body class="d-flex flex-column h-100">
-
-    <?php include 'menu.php'; ?>
-
-    <!-- Contenido -->
-    <main class="flex-shrink-0">
-        <div class="container">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-                <?php foreach ($resultado as $row) { ?>
-                    <div class="col mb-2">
-                        <div class="card shadow-sm h-100">
-
-                            <?php
-                            $id = $row['id'];
-                            $imagen = "images/productos/$id/principal.jpg";
-
-                            if (!file_exists($imagen)) {
-                                $imagen = "images/no-photo.jpg";
-                            }
-                            ?>
-                            <a href="details.php?id=<?php echo $row['id']; ?>&token=<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>">
-                                <img src="<?php echo $imagen; ?>" class="card-img-top">
-                            </a>
-
-                            <div class="card-body">
-                                <p class="card-title"><?php echo $row['nombre']; ?></p>
-                                <p class="card-text"><strong><?php echo MONEDA . ' ' . number_format($row['precio'], 2, '.', ','); ?></strong></p>
-                            </div>
-                            <div class="card-footer bg-transparent">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <a href="details.php?id=<?php echo $row['id']; ?>&token=<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>" class="btn btn-primary">Detalles</a>
-                                    </div>
-                                    <a class="btn btn-success" onClick="addProducto(<?php echo $row['id']; ?>, '<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>')">Agregar</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
+    <style>
+    body {
+        background-image: url('images/sp1.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 100vh;
+        margin: 0;
+        padding: 0;
+    }
+</style>
+<body>
+<?php include 'menu.php'; ?>
+         
+<div class="p-3 mb-2 bg-gradient-primary text-white d-flex flex-column justify-content-center align-items-center" style="height: 90vh;">
+    <div class="d-flex flex-column align-items-center">
+        <h1 class="text-center">Sport Shop Online</h1>
+        <br>
+        <p class="lead text-center">Aprovecha las nuevas ofertas por lanzamiento oficial de la página. ¡Entra ahora!</p>
+        <br>
+        <div class="mt-auto">
+            <button class="btn btn-primary" type="button" onclick="window.location.href=('catalogo.php')">Entrar</button>
         </div>
-    </main>
-
-    <?php include 'footer.php'; ?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script>
-        function addProducto(id, token) {
-            var url = 'clases/carrito.php';
-            var formData = new FormData();
-            formData.append('id', id);
-            formData.append('token', token);
-
-            fetch(url, {
-                    method: 'POST',
-                    body: formData,
-                    mode: 'cors',
-                }).then(response => response.json())
-                .then(data => {
-                    if (data.ok) {
-                        let elemento = document.getElementById("num_cart")
-                        elemento.innerHTML = data.numero;
-                    }
-                })
-        }
-    </script>
+    </div>
+</div>
+script src="js/bootstrap.min.js"></script>
 </body>
-
 </html>
--- menu.php
+++ menu.php
@@ -10,7 +10,7 @@
             <div class="collapse navbar-collapse" id="navBarTop">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Catalogo</a>
+                        <a class="nav-link active" href="catalogo.php">Catalogo</a>
                     </li>
 
                     <li class="nav-item">
