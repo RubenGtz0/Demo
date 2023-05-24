4099eb20f5cbcd2fe1da7d5f8f8a0bbdfd857a24
RubenGtz0 <gutierrezruben.000@gmail.com>
2023-05-23 20:41:40 -0600

    Actualizacion 3.1/4

--- config/config.php
+++ config/config.php
@@ -10,12 +10,12 @@ define("KEY_TOKEN", "TU_TOKEN");
 define("MONEDA", "$");
 
 //Configuración para Paypal
-define("CLIENT_ID", "TU_CLIENT_ID_PAYPAL");
+define("CLIENT_ID", "Abld8JVwFUQmfVc6Yws5XOKPe_25MSXGCQhM2d9pV8_l0hEp16VXhI9hSg44HSt88TlbgeCYK3WCZDPm");
 define("CURRENCY", "MXN");
 
 //Configuración para Mercado Pago
-define("TOKEN_MP", "TEST-XXXXXXXXX");
-define("PUBLIC_KEY_MP", "TEST-XXXXXXXXX");
+define("TOKEN_MP", "TEST-8841373638935141-052322-d1a90f113abc318cb7a7bbd014d839a2-250471414");
+define("PUBLIC_KEY_MP", "TEST-9c9d468d-5357-4dd7-a35e-420299b12b92");
 define("LOCALE_MP", "es-MX");
 
 
